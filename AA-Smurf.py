b712731b7e9ede93a024083d5d4fd3bb07d3724d
mengchillee <jeremy08300830@gmail.com>
2020-07-27 04:23:45 -0400

    Add files via upload

--- AA-Smurf.py
+++ AA-Smurf.py
@@ -78,7 +78,7 @@ def compute_mdl(ajm, order, start, count):
 
 	return mdl, np.mean(purity)
 
-def AA_Smurf(ajm, c, max_iter, visualize):
+def AA_Smurf(edgelist, c, max_iter, visualize, ans):
 	"""
 	Identify the best order for spotting smurf pattern
 
@@ -91,7 +91,7 @@ def AA_Smurf(ajm, c, max_iter, visualize):
 	OUTPUTS
 	[Reordered matrix, Best order for reordering]
 	"""
-
+	ajm, node_dict = edgelist_to_matrix(edgelist)
 	### Get edge-pairs which have the number of intermediaries hgiher than the threshold c
 	print('Get Edge-Pairs...')
 	row, col = ajm, ajm.T
@@ -106,47 +106,57 @@ def AA_Smurf(ajm, c, max_iter, visualize):
 
 	### Heuristically identify the best order by MDL and purity
 	print('Identify Best Order...')
-	n = len(ajm)
-	old_mdl = np.ceil(np.sum(1 - ajm)) * (2 * ceil(np.log2(n)))
+	def func(ajm, key, value, order, count, start, prev_mdl):
+		if key[0] not in order and key[1] not in order:
+			order.append(key[0])
+			tmp_mid = [a for a in value[1] if a not in order]
+			if len(tmp_mid) == 0:
+				return -1, -1, -1, -1, -1
+			order.extend(tmp_mid)
+			order.append(key[1])
+			start.append(len(order))
+			mdl, purity = compute_mdl(ajm, copy.copy(order), copy.copy(start),
+								[count[0] + 1, count[1] + len(tmp_mid), count[2] + 1])
+			score = ((prev_mdl - mdl) / prev_mdl) * purity
+
+			if mdl < prev_mdl:
+				count = [count[0] + 1, count[1] + len(tmp_mid), count[2] + 1]
+				return mdl, score, order, start, count
+		return -1, -1, -1, -1, -1
+
+	old_mdl = np.ceil(np.sum(1 - ajm)) * (2 * ceil(np.log2(len(ajm))))
 	count_arr, order_arr, start_arr, mdl_arr = [[0, 0, 0]], [[]], [[0]], [old_mdl]
-	length = len(edis.keys())
 	iter = 0
 	while True:
-		stop = True
-		tmp_mdl, prev_score = mdl_arr[-1], 0
+		prev_mdl, prev_score = mdl_arr[-1], 0
+		tmp_mdl, tmp_score, tmp_order, tmp_start, tmp_count = [], [], [], [], []
 		for idx, (key, value) in enumerate(edis.items()):
-			tmp_order = copy.copy(order_arr[-1])
-			if key[0] not in tmp_order and key[1] not in tmp_order:
-				tmp_count, tmp_start = count_arr[-1], copy.copy(start_arr[-1])
-				tmp_order.append(key[0])
-				tmp_mid = [a for a in value[1] if a not in tmp_order]
-				if len(tmp_mid) == 0:
-					continue
-				tmp_order.extend(tmp_mid)
-				tmp_order.append(key[1])
-				tmp_start.append(len(tmp_order))
-				mdl, purity = compute_mdl(copy.copy(ajm), copy.copy(tmp_order), copy.copy(tmp_start),
-									[tmp_count[0] + 1, tmp_count[1] + len(tmp_mid), tmp_count[2] + 1])
-
-				score = ((tmp_mdl - mdl) / tmp_mdl) * purity
-				if mdl < tmp_mdl and score > prev_score:
-					prev_score = score
-					order, start, ori_mdl = copy.copy(tmp_order), copy.copy(tmp_start), mdl
-					count = [tmp_count[0] + 1, tmp_count[1] + len(tmp_mid), tmp_count[2] + 1]
-					stop = False
+			mdl, score, order, start, count = func(copy.copy(ajm), \
+												   key, value, \
+												   copy.copy(order_arr[-1]), \
+												   copy.copy(count_arr[-1]), \
+												   copy.copy(start_arr[-1]),
+												   prev_mdl)
+			tmp_mdl.append(mdl)
+			tmp_score.append(score)
+			tmp_order.append(order)
+			tmp_start.append(start)
+			tmp_count.append(count)
 
 		### No more smurf-like pattern found
-		if stop or (max_iter != None and iter > max_iter):
+		if np.max(tmp_score) == -1 or (max_iter != None and iter > max_iter):
 			break
-		count_arr.append(count)
-		order_arr.append(order)
-		start_arr.append(start)
-		mdl_arr.append(ori_mdl)
+		max_idx = np.argmax(tmp_score)
+		count_arr.append(tmp_count[max_idx])
+		order_arr.append(tmp_order[max_idx])
+		start_arr.append(tmp_start[max_idx])
+		mdl_arr.append(tmp_mdl[max_idx])
 		iter += 1
 	print('Done!\n')
 
 	### Get the result with MDL 10% higher than the minimum bits
 	max_idx = next(idx - 1 for idx, m in enumerate(mdl_arr) if m < mdl_arr[-1] * 1.1)
+
 	count, order, start, ori_mdl = count_arr[max_idx], order_arr[max_idx], start_arr[max_idx], mdl_arr[max_idx]
 	order.extend([i for i in range(n) if i not in order])
 	ro_ajm = ajm[np.ix_(order, order)]
@@ -167,18 +177,16 @@ def AA_Smurf(ajm, c, max_iter, visualize):
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Parameters for AA-Smurf of AutoAudit')
-	parser.add_argument('--f', default='data/sample_matrix.txt', type=str, help='Input Path')
+	parser.add_argument('--f', default='data/cfd_injected5.pkl', type=str, help='Input Path')
 	parser.add_argument('--o', default='results/AA-Smurf_result.png', type=str, help='Output Path')
-	parser.add_argument('--c', default=8, type=int, help='Intermediaries Threshod')
+	parser.add_argument('--c', default=3, type=int, help='Intermediaries Threshod')
 	parser.add_argument('--i', default=None, type=int, help='Maximum Iteration')
 	args = parser.parse_args()
 
-	ajm = np.loadtxt(args.f)
-	ro_ajm, order = AA_Smurf(ajm, args.c, args.i, args.o)
+	with open(args.f, 'rb') as handle:
+		data = pickle.load(handle)
 
-	# with open(args.f, 'rb') as handle:
-	# 	data = pickle.load(handle)
-	# for k, v in data.items():
-	# 	for idx, vv in enumerate(v):
-	# 		ajm, node_dict = edgelist_to_matrix(vv['Edgelist'])
-	# 		ro_ajm, order = AA_Smurf(ajm, args.c, args.i, args.o)
+	for k, v in data.items():
+		s = 0
+		for idx, vv in enumerate(v):
+			ro_ajm, order = AA_Smurf(vv['Edgelist'], args.c, args.i, args.o, vv['Label'])
